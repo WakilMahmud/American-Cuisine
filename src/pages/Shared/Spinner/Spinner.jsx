830dda722e65d63e41949a4afdc3cca6ec34419a
WakilMahmud <wakilmahmud30@gmail.com>
2023-05-04 00:24:39 +0600

    Auth Provider is added

---
+++ src/pages/Shared/Spinner/Spinner.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const Spinner = () => {
+	return (
+		<div className="flex justify-center items-center">
+			<div className="w-10 h-10  border-dotted border-4 border-blue-500  rounded-full animate-spin"></div>
+		</div>
+	);
+};
+
+export default Spinner;
