a4e0974734edf3f1808522f41c5f6afb3ef66c3d
WakilMahmud <wakilmahmud30@gmail.com>
2023-05-04 09:12:15 +0600

    Register Page Form Validation & Create User

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,10 +3,11 @@ import React from "react";
 import Navbar from "../pages/Shared/Navbar/Navbar";
 import Footer from "../pages/Shared/Footer/Footer";
 import { Outlet } from "react-router-dom";
-
+import { Toaster } from "react-hot-toast";
 const MainLayout = () => {
 	return (
 		<>
+			<Toaster></Toaster>
 			<Navbar></Navbar>
 			<Outlet></Outlet>
 			<Footer></Footer>
--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,12 +1,76 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
+import toast from "react-hot-toast";
 
 const Register = () => {
+	const [emailError, setEmailError] = useState("");
+	const [passwordError, setPasswordError] = useState("");
+	const { createUser } = useContext(AuthContext);
+
+	const handleRegister = (event) => {
+		event.preventDefault();
+		const form = event.target;
+		const name = form.name.value;
+		const photo = form.photo.value;
+		const email = form.email.value;
+		const password = form.password.value;
+
+		console.log(name, photo, email, password);
+
+		// Form validation
+		if (email === "") {
+			setEmailError("Please enter your email address.");
+			return;
+		} else {
+			setEmailError("");
+		}
+		if (password === "") {
+			setPasswordError("Please enter your password.");
+			return;
+		} else if (password.length < 6) {
+			setPasswordError("Password must be at least 6 characters.");
+			return;
+		} else {
+			setPasswordError("");
+		}
+
+		createUser(email, password)
+			.then((result) => {
+				const createdUser = result.user;
+				console.log(createdUser);
+				toast.success("Successfully Registered!");
+			})
+			.catch((error) => {
+				console.log(error);
+				// toast.error(error.message);
+				toast.error("Email is already in use!");
+			});
+
+		// updateProfile(auth.currentUser, {
+		// 	displayName: "Jane Q. User",
+		// 	photoURL: "https://example.com/jane-q-user/profile.jpg",
+		// })
+		// 	.then(() => {
+		// 		// Profile updated!
+		// 		// ...
+		// 	})
+		// 	.catch((error) => {
+		// 		// An error occurred
+		// 		// ...
+		// 	});
+
+		form.name.value = "";
+		form.email.value = "";
+		form.password.value = "";
+		form.photo.value = "";
+	};
+
 	return (
 		<div className="flex flex-col items-center justify-center h-screen ">
 			<h2 className="text-2xl font-bold mb-4">Register</h2>
-			<form className="w-full max-w-md">
+			<form className="w-full max-w-md" onSubmit={handleRegister}>
 				<div className="mb-4">
 					<label htmlFor="name" className="block text-gray-700 font-bold mb-2">
 						Name
@@ -22,26 +86,29 @@ const Register = () => {
 					<label htmlFor="email" className="block text-gray-700 font-bold mb-2">
 						Email
 					</label>
+					{emailError && <p className="text-red-500 text-xs italic mt-2">{emailError}</p>}
 					<input
 						type="email"
 						id="email"
 						name="email"
 						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-						required
 					/>
 				</div>
+
 				<div className="mb-6">
 					<label htmlFor="password" className="block text-gray-700 font-bold mb-2">
 						Password
 					</label>
+					{passwordError && <p className="text-red-500 text-xs italic mt-2">{passwordError}</p>}
+
 					<input
 						type="password"
 						id="password"
 						name="password"
 						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-						required
 					/>
 				</div>
+
 				<div className="mb-4">
 					<label htmlFor="photo" className="block text-gray-700 font-bold mb-2">
 						Photo URL
