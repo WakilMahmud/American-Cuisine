2c97acd7bcfc869e7d7f99acdd5c86955020111c
WakilMahmud <wakilmahmud30@gmail.com>
2023-05-05 19:46:20 +0600

    Error Page UI is updated

--- src/pages/Shared/ErrorPage/ErrorPage.jsx
+++ src/pages/Shared/ErrorPage/ErrorPage.jsx
@@ -9,7 +9,7 @@ const ErrorPage = () => {
 	return (
 		<section className="flex items-center h-screen p-16 bg-gray-100 text-gray-900">
 			<div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
-				<Lottie animationData={Error} loop={true} />;
+				<Lottie animationData={Error} loop={true} />
 				<div className="max-w-md text-center">
 					<p className="text-2xl font-semibold md:text-3xl mb-8">{error?.message}</p>
 					<p className="text-2xl font-semibold md:text-3xl mb-8 text-red-500 uppercase">{statusText}</p>
